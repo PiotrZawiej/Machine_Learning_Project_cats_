a0df7ade4a3903cb6f5ce409a10ba6c8cc060651
Skirall <krzysztof.zdonek@edu.uekat.pl>
2023-10-25 14:48:26 +0200

    Zlicza dystans między punktami

--- zadanie_2_gotowe.py
+++ zadanie_2_gotowe.py
@@ -16,25 +16,29 @@ class Point:
 board_width = 5000
 board_height = 5000
 
+#answer = input("Czy chcesz stworzyć własne punkty? (Y/N)")
+
 #Tworzenie randomowychpunktów na planszy
 def random_point(width, height):
-    return Point(random.randint(0, width - 1), random.randint(0, height - 1))
+    return Point(random.randint(0, width), random.randint(0, height))
 
 #Początkowy i końcowy punkt
 start_point = random_point(board_width, board_height)
 end_point = random_point(board_width, board_height)
 
 #Liczba punktów
-num_points_n = random.randint(1, 4999)
+num_points_n = random.randint(1, 5000)
 points_n = [random_point(board_width, board_height) for _ in range(num_points_n)]
 
 # Losowanie punktów k
 k = random.randint(1, num_points_n)
 points_to_visit = points_n[:k]
 
+
 #Obliczanie dystansu przez pitagorasa
 def distance(point1, point2):
     return math.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2)
+
 #Dla Astar do szacowania długości między punktami
 def heuristic(point):
     return distance(point, end_point)
@@ -97,6 +101,7 @@ def astar(start, end):
 def find_optimal_path(start, end, points_to_visit):
     path = []
     current_point = start
+
     for point_to_visit in points_to_visit:
         #Tutaj wywoływany jest Astar dla punktów do odiwedzenia
         result = astar(current_point, point_to_visit)
@@ -130,6 +135,18 @@ print(f"({start_point.x}, {start_point.y})")
 print("Punkt końcowy: ")
 print(f"({end_point.x}, {end_point.y})")
 
+total_distance=0
+# Konwertowanie punktów na int
+points_to_visit_int = [Point(int(point.x), int(point.y)) for point in points_to_visit]
+# Obliczanie dystansu
+for i in range(len(points_to_visit_int) - 1):
+    point1 = points_to_visit_int[i]
+    point2 = points_to_visit_int[i + 1]
+    distance_between_points = distance(point1, point2)
+    total_distance += distance_between_points
+
+print(f"Łączna odległość między punktami: {total_distance}")
+
 #Zapisywanie do pliku
 file_name = open("Wynik.csv","w")
 file_name.write(f'Punkt poczatkowy: {start_point.x}, {start_point.y}\n')
@@ -139,5 +156,6 @@ file_name.write(f'Liczba punktow k: {k}\n')
 file_name.write(f"Punkty do odiwedzenia: \n")
 for points in points_to_visit:
     file_name.write(f"{point.x},{point.y};")
+file_name.write(f"Łączna długość ścieżki: {total_distance}")
 file_name.close()
 
