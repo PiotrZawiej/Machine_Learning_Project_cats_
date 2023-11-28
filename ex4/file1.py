022baa904001167978f92b75492e10dc9c771605
piotr zawiej <piotr.zawieja0x@gmail.com>
2023-11-28 14:56:53 +0100

    first class cat

--- ex4/file1.py
+++ ex4/file1.py
@@ -1,1 +1,17 @@
-print("AAAAAAA")
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 28 13:25:42 2023
+
+@author: Dell
+"""
+
+
+from classCat import cat
+import numpy as np
+
+arr = np.array([["X" for x in range(50)] for x in range(50)])
+print(arr[49,49])
+
+Luna = cat("Luna", "snail")
+
+print(Luna.name, Luna.best_object)
