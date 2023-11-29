611d1745d851d52e798896a3f7af1bbea75657e3
piotr zawiej <piotr.zawieja0x@gmail.com>
2023-11-29 13:08:49 +0100

    smth wrong

---
+++ ex4/ObjectClass.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 29 10:05:26 2023
+
+@author: Dell
+"""
+
+class pickObject:
+    
+    objectName = ""
+    quanity = 0
+    
+    def __init__(self, on, q):
+        self.objectName = on
+        self.quanity = q
+        
--- ex4/file1.py
+++ ex4/file1.py
@@ -7,11 +7,20 @@ Created on Tue Nov 28 13:25:42 2023
 
 
 from classCat import cat
+from ObjectClass import pickObject
 import numpy as np
 
-arr = np.array([["X" for x in range(50)] for x in range(50)])
-print(arr[49,49])
+def spawnObjectMap(arr, objects):
+    for obj in objects:
+        
+        x, y = np.random.randint(0, arr.shape[0]), np.random.randint(0, arr.shape[1])
+        
+        arr[x, y] = obj.objectName
+        
+arr = np.array([[ "X" for x in range(50)] for x in range(50)])
 
 Luna = cat("Luna", "snail")
+Ariana = cat("Ariana", "rock")
+Dante = cat("Dante", "mouse")
 
-print(Luna.name, Luna.best_object)
+arr[int(len(arr)/2), int(len(arr[0])/2)] = "Luna"
