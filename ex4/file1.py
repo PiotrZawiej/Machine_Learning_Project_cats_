a7ac3b698d6be26657a63948a6479f1c3b7606b3
PiotrZawiej <piotr.zawieja0x@gmail.com>
2023-12-06 23:54:02 +0100

    map generator

--- ex4/file1.py
+++ ex4/file1.py
@@ -10,17 +10,27 @@ from classCat import cat
 from ObjectClass import pickObject
 import numpy as np
 
-def spawnObjectMap(arr, objects):
-    for obj in objects:
-        
-        x, y = np.random.randint(0, arr.shape[0]), np.random.randint(0, arr.shape[1])
-        
-        arr[x, y] = obj.objectName
-        
-arr = np.array([[ "X" for x in range(50)] for x in range(50)])
+
 
 Luna = cat("Luna", "snail")
 Ariana = cat("Ariana", "rock")
 Dante = cat("Dante", "mouse")
 
-arr[int(len(arr)/2), int(len(arr[0])/2)] = "Luna"
+cats = [Luna.name, Ariana.name, Dante.name]
+
+def map_generator(map_size, house_size):
+    arr = np.array([["X" for x in range(map_size)] for x in range(map_size)])
+    center_x, center_y = (len(arr[0]) // 2, len(arr[0]) // 2)
+    left_top_x = center_x - (house_size // 2)
+    left_top_y = center_y - (house_size // 2)
+    arr[left_top_y:left_top_y + house_size, left_top_x:left_top_x + house_size] = 'H'
+    return arr
+
+arr = map_generator(50, 10)
+
+
+
+# def hunting(cats, objects, time, arr):
+#     for cat in cats:
+#         time = 2
+#         while time > 0 and objects > 0:
