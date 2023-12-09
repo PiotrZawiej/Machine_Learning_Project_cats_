c5d53ddfa27cc8d056d8f1d83fa549b36bfd2add
Maciook <m4ciej.w4lus@gmail.com>
2023-12-09 20:00:07 +0100

    Added the Backpack class along with its constructor

--- ZAD3/zadanie3.py
+++ ZAD3/zadanie3.py
@@ -2,8 +2,8 @@ import random
 # Wymiary plecaka: 10cmx20cmx10cm, 2000cm^3
 # Wymiary zdobyczy: Mysz Polna 5x3x3, Mysz Domowa 7x2x2, Ślimak 3x3x3, Liść 3x2x1, Kamyk 2x2x1
 # Preferencje kotów: Luna - (MP 0.4, MD 0.4, Ś 0.1, K 0.1 L 0.0), Ariana - (Ś 0.375, L 0.375, MD 0.125, MP 0.125, K 0.0), Dante (K 0.5, MD 0.2, MP 0.2, Ś 0.05 L 0.05)
-
-#Metoda losująca złapaną przez kota zdobycz (Bazowo 0.2 szansy na każdą możliwą zdobycz)
+#
+# Metoda losująca złapaną przez kota zdobycz (Bazowo 0.2 szansy na każdą możliwą zdobycz)
 def catch_prey():
     selection = random.randint(1, 10)
     match selection:
@@ -20,8 +20,19 @@ def catch_prey():
         case _:
             return -1
 
-def create_backpack():
-    pass
+
+class Backpack:
+
+    def __init__(self, x=10, y=20, z=10):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.content = dict()
+
+    def store_prey(self):
+        pass
+
 
 for i in range(10):
     print(catch_prey())
+
